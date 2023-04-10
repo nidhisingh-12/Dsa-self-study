0362f10994b8a635a2bb9bd2a97c0a8ebddc3627
Nidhi Singh <93303985+nidhisingh-12@users.noreply.github.com>
2023-04-10 23:49:46 +0530

    fibonacci series

---
+++ practice.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, t1=0, t2 = 1, nextterm = 0;
+    cout<<"enter the number of terms:";
+    cin>>a;
+
+    cout<<"Fibonacci Series: ";
+
+    for(int i=1; i<=a; ++i)
+    {
+       if(i==1)
+       {
+        cout<<t1<<" ,";
+        continue;
+       }
+       if(i==2)
+       {
+        cout<<t2<<" ,";
+        continue;
+       }
+        
+    nextterm = t1+t2;
+    t1 = t2;
+    t2 = nextterm;
+
+    cout<<nextterm<<",";
+    }
+    
+    return 0;
+   
+}
