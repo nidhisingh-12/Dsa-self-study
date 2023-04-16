393b06dbe8b7907f3dd3bd0ecf6806f7857723a2
Nidhi Singh <93303985+nidhisingh-12@users.noreply.github.com>
2023-04-16 17:04:22 +0530

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -41,19 +41,125 @@ int main()
     // }   
 
     /*----print natural number in square----*/
-    int num;
-    cin>>num;
-    int count = 1;
+    // int num;
+    // cin>>num;
+    // int count = 1;
 
-    for(int i=1; i<=num; i++)
-    {
-        for(int j=1; j<=num; j++)
-        {
-            cout<<count<<" ";
-            count = count+1;
+    // for(int i=1; i<=num; i++)
+    // {
+    //     for(int j=1; j<=num; j++)
+    //     {
+    //         cout<<count<<" ";
+    //         count = count+1;
 
-        }
-        cout<<endl;
-    }
+    //     }
+    //     cout<<endl;
+    // }
+
+    /*---------half pyramid----------*/
+
+    // int a;
+    // cin>>a;
+    // for(int row =1; row<=a; row++)
+    // {
+    //     for(int col=1; col<=row; col++)
+    //     {
+    //         cout<<"*"<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    /*-------half pyramid using number(1,22,333)*/
+    // int a;
+    // cin>>a;
+    // for(int row =1; row<=a; row++)
+    // {
+    //     for(int col=1; col<=row; col++)
+    //     {
+    //         cout<<row<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    /*--------half pyrmaid using number(1,23,456)--------*/
+    // int a;
+    // cin>>a;
+    // for(int row =1; row<=a; row++)
+    // {
+    //     for(int col=1; col<=row; col++)
+    //     {
+    //         cout<<"*"<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    /*-------half pyrmaid using number in reverse order (1,21,321,4321)------------*/
+    // int x;
+    // cin>>x;
+
+    // for(int i=1; i<=x; i++)
+    // {
+    //     for(int j=1; j<=i; j++)
+    //     {
+    //         cout<<(i-j+1)<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    /*----rectangle  using alphabet (AAA,BBB,CCC)--*/
+    // int t;
+    // cin>>t;
+    // for(int i=1; i<=t; i++)
+    // {
+        
+    //     for(int j=1; j<=t; j++)
+    //     {
+    //         char ch = 'A'+i-1;
+    //         cout<<ch;
+    //     }
+    //     cout<<endl;
+    // }
+
+    /*-----rectangle using alphabet(ABC,ABC,ABC) */
+    // int e;
+    // cin>>e;
+    // for(int i=1; i<=e; i++)
+    // {
+    //     for(int j=1; j<=e; j++)
+    //     {
+    //        char ch = 'A'+j-1;
+    //        cout<<ch <<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+   /*---half pyrmaid (A,BC,CDE,DEFG) ---*/
+//    int n;
+//    cin>>n;
+   
+//    for(int row = 1; row<=n; row++)
+//    {
+//     for(int col=1; col<=row; col++)
+//     {
+//         char ch = ('A' + row+col-2);
+//         cout<<ch;
+        
+//     }
+//     cout<<endl;
+//    }  
+/*---half pyramid (D,CD,BCD,ABCD)--*/
+    // int t;
+    // cin>>t;
+    // for(int i=1; i<=t; i++)
+    // {
+    //     char ch = 'A'+t-i;
+    //     for(int j=1; j<=i; j++)
+    //     {
+    //        cout<<ch;
+    //        ch=ch+1;
+    //     }
+    //     cout<<endl;
+    // }
+
+    
     return 0;
 }
